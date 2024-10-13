c31a91c35578cc31794055cff93ca8783a124bbf
William Bellavance Jr <flintman@flintmancomputer.com>
2024-10-13 11:08:57 -0400

    Move over to Evervolv
    Change-Id: I4a3ff41cc9f68b574eee3b1adb3a36588b436a7d

--- BoardConfigEv.mk
+++ BoardConfigEv.mk
@@ -9,7 +9,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := true
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
 
 # Manifests
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += hardware/lineage/interfaces/compatibility_matrices/compatibility_matrix.lineage.xml
 DEVICE_MANIFEST_FILE += device/google/gs101/manifest_radio.xml
 
 # Partitions
@@ -32,7 +32,7 @@ AB_OTA_PARTITIONS += \
     vbmeta_vendor
 
 # Reserve space for gapps install
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include $(SRC_EVERVOLV_DIR)/build/target/board/BoardConfigReservedSize.mk
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
