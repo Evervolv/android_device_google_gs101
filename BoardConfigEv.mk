f9408e921bd6ca2a0e12fac540c4b7110a7a000c
Nick Reuter <nreuter85@gmail.com>
2025-05-16 20:18:33 -0500

    gs101: drop compatibility_matrix.evervolv.xml
    Change-Id: I3074049c6552b8ef4cfe34621fbf1cb6415cc32f

--- BoardConfigEv.mk
+++ BoardConfigEv.mk
@@ -6,7 +6,6 @@
 
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    hardware/evervolv/interfaces/compatibility_matrices/compatibility_matrix.evervolv.xml \
     hardware/lineage/interfaces/compatibility_matrices/compatibility_matrix.lineage.xml
 
 DEVICE_MANIFEST_FILE += device/google/gs101/manifest_radio.xml
